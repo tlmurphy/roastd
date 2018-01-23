e0477171ef12aa9008890c929b4ab061fa19c858
John Hossler <john.m.hossler@adtran.com>
2018-01-23 14:15:54 -0600

    Added another tab, let's see if this works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { AnotherTabPage } from '../pages/another_tab/another_tab';
 import { ListPage } from '../pages/list/list';
 
 @Component({
@@ -22,6 +23,7 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
+      { title: 'Another Tab', component: AnotherTabPage },
       { title: 'List', component: ListPage }
     ];
 
---
+++ src/pages/another_tab/another_tab.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-another_tab',
+  templateUrl: 'another_tab.html'
+})
+export class AnotherTabPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
